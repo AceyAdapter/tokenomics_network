992a5878ade2f94e039c2dd19d0bb526018a4c49
AceyAdapter <nicholaskreitz@gmail.com>
2022-06-23 00:20:59 +0100

    theme

--- .svelte-kit/output/server/nodes/0.js
+++ .svelte-kit/output/server/nodes/0.js
@@ -3,4 +3,4 @@ import * as module from '../entries/pages/__layout.svelte.js';
 export { module };
 export const entry = 'pages/__layout.svelte-80efec3c.js';
 export const js = ["pages/__layout.svelte-80efec3c.js","chunks/index-faf28bcd.js"];
-export const css = ["assets/pages/__layout.svelte-b15ae9ef.css"];
+export const css = ["assets/pages/__layout.svelte-4f6fa6bf.css"];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,20 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["synthwave"],
+    themes: [
+      {
+        mytheme: {     
+          "primary": "#fde047",
+          "secondary": "#b6e1f9",
+          "accent": "#72dd66",
+          "neutral": "#271F28",
+          "base-100": "#324653",
+          "info": "#5A91DD",
+          "success": "#1C9B5C",
+          "warning": "#EDCA4A",
+          "error": "#EF2E5F",
+        },
+      },
+    ],
   },
 }
